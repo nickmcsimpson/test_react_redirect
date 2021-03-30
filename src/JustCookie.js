9e4793ed4e87812e9359f579a0f34d47d020f658
Nicholas McClain-Simpson <n.mcsimp@gmail.com>
2021-03-30 11:19:41 -0500

    whoops wrong origin

--- src/JustCookie.js
+++ src/JustCookie.js
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 function Cookie() {
   useEffect(() => {
-    fetch('http://docker.channelmix.com:5000/auth-urls?clientId=3973&redirectPath=/landing&sourceId=8', {
+    fetch('https://datorama.qa.channelmix.com:5500/auth-urls?clientId=3973&redirectPath=/landing&sourceId=8', {
         credentials: 'include',  
         headers: {
         'Authorization': 'Bearer JWT_TOKEN',
       }
     }).then(() => {
-      fetch('http://docker.channelmix.com:5000/clients?cmxapiConsumerIdentifier=10', {
+      fetch('https://datorama.qa.channelmix.com:5500/clients?cmxapiConsumerIdentifier=10', {
         credentials: 'include',
         headers: {
             'Authorization': 'Bearer JWT_TOKEN',
